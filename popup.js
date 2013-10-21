2472390a8b4893a383d47ec7deefcecf11d628aa
John McMellen <jmcmellen@gmail.com>
2013-10-21 14:20:12 -0500

    New msg format, added regex and date parsing

--- popup.js
+++ popup.js
@@ -7,7 +7,8 @@ var towerIds = ['1007705', '1007704', '1005399'];
 
 var qtyNotamsRegex = new RegExp(".*Number of NOTAMs:\\D*(\\d+)","gim");
 
-var asrNotamsRegex = new RegExp("(lgts ots).*asr.*(\\d{7})\\).*til.*(\\d{10}).*","gim");
+//var asrNotamsRegex = new RegExp("(lgts ots).*asr.*(\\d{7})\\).*til.*(\\d{10}).*","gim");
+var asrNotamsRegex = new RegExp("(obst tower lgt).*asr.*(\\d{7})\\).*out of service.*(\\d{10}\\-\\d{10}).*","gim");
 //var asrNotamsRegex = new RegExp(".*(lgts ots).*asr.*(\\d{7}).*(til)|(wef).*(\\d{10})-?(\\d{10}?)", "gim");
 
 var notamRequestGenerator = {
@@ -34,11 +35,22 @@ var notamRequestGenerator = {
     asrNotamsRegex.lastIndex = 0;
     if (null !== result) {
       var asrnum = result[2];
-      var expire_time = result[3];
+      var valid_time = result[3].split("-");
       text = '<div>' + text + '</div>';
-      text += '<p>ASR is ' + asrnum + '</p>';
+      text += '<p>ASR is ' + asrnum + '<br/> Expires ' + notamRequestGenerator.parseDateStr_(valid_time[1]) + ' </p>';
       $('body').append(text);
     }
+  },
+
+  parseDateStr_: function(str) {
+    var d = new Date();
+    d.setUTCFullYear(parseInt("20" + str.slice(0,2)), parseInt(str.slice(2,4))-1, parseInt(str.slice(4,6)) );
+    //d.setUTCMonth(parseInt(str.slice(2,4)));
+    //d.setUTCDate(parseInt(str.slice(4,6)));
+    d.setUTCHours(parseInt(str.slice(6,8)));
+    d.setUTCMinutes(parseInt(str.slice(8)));
+    return d.toString();
+
   },
 
   findNotams_: function(data) {
